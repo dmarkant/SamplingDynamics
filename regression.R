e6ffbdca68fb0225d6bb6b38333e8dd2fa60018b
Doug Markant <dmarkant@gmail.com>
2014-07-08 14:02:29 +0200

    regression updates

--- regression.R
+++ regression.R
@@ -1,6 +1,7 @@
 setwd("~/code/SamplingDynamics/")
 
 library(lme4)
+library(plyr)
 
 data = as.data.frame(read.csv('reg_data.csv'))
 data$domain = factor(data$domain)
@@ -27,7 +28,75 @@ for (sid in unique(data$partid)) {
   
 }
 
-sdata = data[data$partid==3,]
+sdata = data[data$partid==120,]
+sdata$switch_or_stop = sdata$switched==1 | sdata$stopped==1
+sdata$abs_dev = abs(sdata$deviation)
+
+# SWITCH OR STOP
+
+# check out frequency of switch/stop as a function of streak length
+r = ddply(sdata, c("streak_length"), function(df) mean(df$switch_or_stop))
+names(r) = c("streak_length", "prop")
+plot(r$streak_length, r$prop, ylim=c(0, 1))
+plot(log(r$streak_length), r$prop)
+
+m1 = glm(switch_or_stop ~ 1 + streak_length, data=sdata, family=binomial)
+plot(sdata$streak_length, fitted(m1), ylim=c(0, 1))
+lines(r$streak_length, r$prop, col='blue')
+
+# most recent outcome
+r = ddply(sdata, c("sample_out"), function(df) mean(df$switch_or_stop))
+names(r) = c("sample_out", "prop")
+
+m2 = glm(switch_or_stop ~ 1 + sample_out, data=sdata, family=binomial)
+plot(sdata$sample_out, fitted(m2), ylim=c(0, 1))
+lines(r$sample_out, r$prop, col='blue')
+
+# deviation from sample mean
+r = ddply(sdata, c("deviation"), function(df) mean(df$switch_or_stop))
+names(r) = c("deviation", "prop")
+
+m3 = glm(switch_or_stop ~ 1 + deviation, data=sdata, family=binomial)
+plot(sdata$deviation, fitted(m2), ylim=c(0, .3))
+lines(r$deviation, r$prop, col='blue')
+
+# absolute deviation from sample mean
+
+
+# JUST SWITCH
+
+# streak length -- doesn't make much sense without including stop decisions as well
+
+# most recent outcome
+r = ddply(sdata, c("sample_out"), function(df) mean(as.numeric(df$switched)-1))
+names(r) = c("sample_out", "prop")
+
+m2 = glm(switched ~ 1 + sample_out, data=sdata, family=binomial)
+plot(sdata$sample_out, fitted(m2), ylim=c(0, .5))
+lines(r$sample_out, r$prop, col='blue')
+
+# deviation from sample mean
+r = ddply(sdata, c("deviation"), function(df) mean(as.numeric(df$switched)-1))
+names(r) = c("deviation", "prop")
+plot(r$deviation, r$prop)
+
+m3 = glm(switched ~ 1 + deviation, data=sdata, family=binomial)
+plot(sdata$deviation, fitted(m2), ylim=c(0, .3))
+lines(r$deviation, r$prop, col='blue')
+
+# absolute deviation from sample mean
+
+
+
+
+
+
+
+
+
+
+m1 = glmer(switch_or_stop ~ 1 + (1|gamble_lab), data=sdata, family=binomial)
+m1 = glmer(switch_or_stop ~ 1 + streak_length + (1|gamble_lab), data=sdata, family=binomial)
 
 m1 = glmer(switched ~ 1 + domain + (1|gamble_lab), data=sdata, family=binomial)
 m2 = glmer(switched ~ 1 + domain + streak_length + (1|gamble_lab), data=sdata, family=binomial)
