fc494f94abd027961b07700ab2b286208264363b
Doug Markant <dmarkant@gmail.com>
2014-09-08 15:06:42 +0200

    fitting files

---
+++ discrete_model/fitting.py
@@ -0,0 +1,61 @@
+import numpy as np
+from copy import deepcopy
+
+
+BOUNDS = {'theta': [1, np.inf],
+          'delta': [0, np.inf],
+          'z_temp': [0, np.inf],
+          'prelec_elevation': [0, np.inf],
+          'prelec_gamma': [0, np.inf],
+          'pow_gain': [0, np.inf],
+          'pow_loss': [0, np.inf]}
+
+
+def get_bounds(parname):
+    if '(' in parname:
+        return BOUNDS[parname[:parname.index('(')]]
+    else:
+        return BOUNDS[parname]
+
+
+def pfix(p):
+    return np.min([np.max([p, 1e-5]), 1-(1e-5)])
+
+
+def randstart(parname):
+    if parname is 'theta':
+        return np.random.randint(4, 7)
+    elif parname is 'delta':
+        return 1.
+    elif parname is 'prelec_elevation':
+        return 1.
+    elif parname is 'prelec_gamma':
+        return 1.
+    else:
+        return np.random.random()
+
+
+def unpack(value, args):
+    verbose = args.get('verbose', False)
+    pars = deepcopy(args)
+    fitting = pars['fitting']
+    if verbose: print 'evaluating:'
+    for i, k in enumerate(fitting):
+        pars[k] = value[i]
+        if verbose: print '  %s=%s' % (k, pars[k])
+
+    return pars, fitting, verbose
+
+
+def outside_bounds(pars):
+    outside = False
+    if 'bounds' in pars:
+        bounds = pars['bounds']
+    else:
+
+        bounds = [get_bounds(p) for p in pars['fitting']]
+    for i, k in enumerate(pars['fitting']):
+        if pars[k] < bounds[i][0] or pars[k] > bounds[i][1]:
+            outside = True
+    return outside
+
---
+++ discrete_model/fitting_hau08.py
@@ -0,0 +1,98 @@
+PARSETS = [['theta'],
+           ['theta', 'prelec_elevation'],
+           ['theta', 'prelec_elevation', 'pow_gain'],
+           ['theta', 'prelec_elevation', 'pow_gain', 'pow_loss'],
+           ['theta', 'prelec_gamma'],
+           ['theta', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'prelec_gamma', 'pow_gain', 'pow_loss'],
+           ['theta', 'prelec_elevation', 'prelec_gamma'],
+           ['theta', 'prelec_elevation', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'prelec_elevation', 'prelec_gamma', 'pow_gain', 'pow_loss'],
+           ['theta', 'delta'],
+           ['theta', 'delta', 'prelec_elevation'],
+           ['theta', 'delta', 'prelec_elevation', 'pow_gain'],
+           ['theta', 'delta', 'prelec_elevation', 'pow_gain', 'pow_loss'],
+           ['theta', 'delta', 'prelec_gamma'],
+           ['theta', 'delta', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'delta', 'prelec_gamma', 'pow_gain', 'pow_loss'],
+           ['theta', 'delta', 'prelec_elevation', 'prelec_gamma'],
+           ['theta', 'delta', 'prelec_elevation', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'delta', 'prelec_elevation', 'prelec_gamma', 'pow_gain', 'pow_loss'],
+           ['theta', 'z_temp'],
+           ['theta', 'z_temp', 'prelec_elevation'],
+           ['theta', 'z_temp', 'prelec_elevation', 'pow_gain'],
+           ['theta', 'z_temp', 'prelec_elevation', 'pow_gain', 'pow_loss'],
+           ['theta', 'z_temp', 'prelec_gamma'],
+           ['theta', 'z_temp', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'z_temp', 'prelec_gamma', 'pow_gain', 'pow_loss'],
+           ['theta', 'z_temp', 'prelec_elevation', 'prelec_gamma'],
+           ['theta', 'z_temp', 'prelec_elevation', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'z_temp', 'prelec_elevation', 'prelec_gamma', 'pow_gain', 'pow_loss'],
+           ['theta', 'delta', 'z_temp'],
+           ['theta', 'delta', 'z_temp', 'prelec_elevation'],
+           ['theta', 'delta', 'z_temp', 'prelec_elevation', 'pow_gain'],
+           ['theta', 'delta', 'z_temp', 'prelec_elevation', 'pow_gain', 'pow_loss'],
+           ['theta', 'delta', 'z_temp', 'prelec_gamma'],
+           ['theta', 'delta', 'z_temp', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'delta', 'z_temp', 'prelec_gamma', 'pow_gain', 'pow_loss'],
+           ['theta', 'delta', 'z_temp', 'prelec_elevation', 'prelec_gamma'],
+           ['theta', 'delta', 'z_temp', 'prelec_elevation', 'prelec_gamma', 'pow_gain'],
+           ['theta', 'delta', 'z_temp', 'prelec_elevation', 'prelec_gamma', 'pow_gain', 'pow_loss']
+           ]
+
+fitlog = 'fits_hau_study1_gridTheta.txt'
+
+fitresults_grid_theta = {4: {}, 5: {}, 6: {}}
+
+
+for theta in [5, 4, 6]:
+    for freepar in PARSETS[1:]:
+        
+        freepar_notheta = [pname for pname in freepar if pname!='theta']
+        
+        pars = {'data': fitdata,
+                'fitting': freepar_notheta,
+                'theta': theta,
+                'bounds': [BOUNDS[p] for p in freepar_notheta]
+        }
+        init = map(randstart, freepar_notheta)
+        f = minimize(loglik_across_gambles, init, (pars,), method='Nelder-Mead')
+        b = bic(f, pars)
+
+        fitresults_grid_theta[theta][tuple(freepar)] = [freepar_notheta, f, b]
+
+        
+        outstr = '\nFixed:\ttheta=%s\n' % theta 
+        outstr += 'Fitting:\t'
+        for pname in freepar_notheta:
+            outstr += '%s\t' % pname
+        outstr += '\n'
+        outstr += 'Success:\t%s\n' % f['success']
+        outstr += 'NegLLH :\t%s\n' % f['fun']
+        outstr += 'Value  :\t'
+        for x in f['x']:
+            outstr += '%s\t' % x
+        outstr += '\n'
+        outstr += 'BIC    :\t%s\n' % b
+        print outstr
+        with open(fitlog, 'a') as f:
+            f.write(outstr)
+        
+        p_resp = []
+        for i, fd in enumerate(fitdata):
+            pred_pars = {'data': fd['samplesize'],
+                         'max_T': fd['max_t'],
+                         'theta': theta,
+                         'options': fd['options']}
+            for par_i, par_name in enumerate(freepar_notheta):
+                pred_pars[par_name] = fitresults_grid_theta[theta][tuple(freepar)][1]['x'][par_i]
+            result = run(pred_pars)
+            p_resp.append(result['resp_prob'][1])
+
+        fig, ax = plt.subplots(figsize=(10, 4))
+        ax.plot(range(1,7), [np.mean(choices(1, pid)) for pid in range(6)], '-o', label='observed')
+        ax.plot(range(1,7), p_resp, '-x', label='best fit model')
+        ax.set_xlim([0, 7])
+        ax.set_ylim(0, 1)
+        ax.legend()
+        plt.show()
